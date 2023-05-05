b5877685b08e85ad0b66a7fabbcfe9080d1a7132
jerry <kea.riland@gmail.com>
2023-05-05 13:57:48 +0300

    add chain of responsability

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/if1bonacci/go-design-patterns/internal/services/chainofresponsibility"
 	"github.com/if1bonacci/go-design-patterns/internal/services/decorator"
 	"github.com/if1bonacci/go-design-patterns/internal/services/dependencyinjection"
 	"github.com/if1bonacci/go-design-patterns/internal/services/factorymethod"
@@ -30,4 +31,15 @@ func main() {
 	fmt.Printf("Team: %s", barsa.GetName())
 	fmt.Println()
 	fmt.Printf("Team: %s", kalmar.GetName())
+	fmt.Println()
+
+	// chain of responsibility
+	delivery := &chainofresponsibility.Delivery{}
+
+	payment := &chainofresponsibility.Payment{}
+	payment.SetNext(delivery)
+
+	order := &chainofresponsibility.Order{Id: "abc"}
+	payment.Execute(order)
+
 }
